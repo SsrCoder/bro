4b1a9195a8286e515d13de0097720c1f49ad1331
Qiongju <SsrCoder@gmail.com>
2024-07-23 11:26:47 +0800

    feat(ocr): read from clipboard (#2)

--- bro-ocr/examples/ocr.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> anyhow::Result<()> {
-    let file = "./bro-ocr/examples/asserts/image.png";
-    let ocr = bro_ocr::OCR {
-        path: Some(file.to_string()),
-        ..Default::default()
-    };
-    ocr.run();
-    Ok(())
-}
